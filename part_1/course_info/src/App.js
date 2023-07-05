d4a6f82e0e2ef0d08baca58002eff04e21aac77c
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2023-07-05 11:31:09 +0100

    step2

--- part_1/course_info/src/App.js
+++ part_1/course_info/src/App.js
@@ -35,17 +35,11 @@ const Header = (props) => {
 
 const Content = (props) => {
     return (
-        <>
-            <p>
-                {props.part1} {props.exercises1}
-            </p>
-            <p>
-                {props.part2} {props.exercises2}
-            </p>
-            <p>
-                {props.part3} {props.exercises3}
-            </p>
-        </>
+        <div>
+            <Part part={props.part1} exercises={props.exercises1} />
+            <Part part={props.part2} exercises={props.exercises2} />
+            <Part part={props.part3} exercises={props.exercises3} />
+        </div>
     );
 };
 
@@ -57,5 +51,13 @@ const Total = (props) => {
                 {props.exercises1 + props.exercises2 + props.exercises3}
             </p>
         </>
+    );
+};
+
+const Part = (props) => {
+    return (
+        <p>
+            {props.part} {props.exercises}
+        </p>
     );
 };
